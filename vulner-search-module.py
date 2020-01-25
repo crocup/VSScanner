a37d1d15594c7d58219e8d0c46efdac92c4d6d3d
dimon <patunutap@gmail.com>
2020-01-25 20:22:51 +0300

    fixed error

--- vulner-search-module.py
+++ vulner-search-module.py
@@ -3,16 +3,15 @@ import os
 import logging.config
 import configparser
 import pymongo
-from components.search_vulnerability import VulnerSearch, search_circl
+from components.search_vulnerability import VulnerabilitySearch, search_circl
 from components.record_database import RecordMongo
-from pprint import pprint
 
 _log_path = "logs/"
 if not os.path.exists(_log_path):
-    os.mkdir(_log_path, 0o755)
+    os.mkdir(_log_path)
 _setting_path = "settings/"
 if not os.path.exists(_setting_path):
-    os.mkdir(_setting_path, 0o755)
+    os.mkdir(_setting_path)
 
 # read settings file
 _path = "settings/settings_VulnerSearchModule.conf"
@@ -29,32 +28,16 @@ db = client[config.get("DATABASE_SCANNER", "BASE")]
 coll = db[config.get("DATABASE_SCANNER", "COLLECTION")]
 
 
-def find_status(product):
-    """
-
-    :param product:
-    :return:
-    """
-    products = str(product).split()
-    products = products[0].lower()
-    if products in cpe:
-        status = True
-    else:
-        status = False
-    return status
-
-
 if __name__ == "__main__":
     ip = config.get("SETTING", "TARGET")
     result = coll.find_one({"ip": ip})
     record_in_mongo = RecordMongo(db=config.get("DATABASE_SCANNER", "BASE"),
                                   coll=config.get("DATABASE_SCANNER", "COLLECTION"))
-    vulnerabilities_api = VulnerSearch(vulners_api=config.get("VULNERS", "API"))
+    vulnerabilities_api = VulnerabilitySearch(vulners_api=config.get("VULNERS", "API"))
     for port in result['result_scan']['tcp']:
         cpe = result['result_scan']['tcp'][str(port)]['cpe']
         product = result['result_scan']['tcp'][str(port)]['product']
         version = result['result_scan']['tcp'][str(port)]['version']
-        print(product + " " + version)
         # Get now data
         now = datetime.datetime.now()
         # get CVE
